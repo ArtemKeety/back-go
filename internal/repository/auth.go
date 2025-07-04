355c16675d032152b3d8cb577362dce30fb65d07
ArtemKetty <artmedvemedvedev@yandex.ru>
2025-07-04 17:52:03 +0300

    thirth v1.1

--- internal/repository/auth.go
+++ internal/repository/auth.go
@@ -16,11 +16,11 @@ func NewAuthRepo(db *sql.DB) *AuthRepository {
 	return &AuthRepository{db: db}
 }
 
-func (a AuthRepository) AddUser(ctx context.Context, u model.UserRequest) (string, error) {
+func (r *AuthRepository) AddUser(ctx context.Context, u model.UserRequest) (string, error) {
 	query := "INSERT INTO users (guid, login, email, password) VALUES ($1, $2, $3, $4)"
 	guid := uuid.New().String()
 
-	_, err := a.db.ExecContext(ctx, query, guid, u.Login, u.Email, u.Password)
+	_, err := r.db.ExecContext(ctx, query, guid, u.Login, u.Email, u.Password)
 	if err != nil {
 		return "", err
 	}
@@ -28,9 +28,9 @@ func (a AuthRepository) AddUser(ctx context.Context, u model.UserRequest) (strin
 	return guid, nil
 }
 
-func (a AuthRepository) CheckUserExists(ctx context.Context, u model.UserRequest) (bool, error) {
+func (r *AuthRepository) CheckUserExists(ctx context.Context, u model.UserRequest) (bool, error) {
 	var guid string
-	row := a.db.QueryRowContext(ctx, "SELECT guid FROM users WHERE email = $1 OR login = $2", u.Email, u.Login)
+	row := r.db.QueryRowContext(ctx, "SELECT guid FROM users WHERE email = $1 OR login = $2", u.Email, u.Login)
 	if err := row.Scan(&guid); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return false, nil
